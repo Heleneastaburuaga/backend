d94cdd74be596f1500d84f2a5920824db43eaa35
Helene Astaburuaga <127048528+Heleneastaburuaga@users.noreply.github.com>
2023-10-23 13:31:26 +0000

    aldaketak

---
+++ app.js
@@ -0,0 +1,21 @@
+let express = require('express');
+let app = express();
+
+app.use(express.static('public')); //middleware
+app.use(express.urlencoded({ extended: false })); //Post parametroak jasotzeko gaitasuna
+
+app.listen( 3002, function() {
+    console.log("Zerbitzaria 3002 portuan entzuten");
+})
+
+app.get("/", function(req, res) {
+    res.send("Hello world!");
+});
+
+app.post("/bezeroa", function(req,res) {
+    res.send(`Kaixo ${req.body.izena}, eskaera jaso dut`);
+});
+
+app.get('/bezeroa/', function(req,res) {
+    res.send(`Bezeroaren izena: ${req.query.izena} ${req.query.abizena}`);
+});
