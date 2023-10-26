7c92706c2efcfe35e18ba57615074976a03f20b3
Helene Astaburuaga <127048528+Heleneastaburuaga@users.noreply.github.com>
2023-10-26 17:59:03 +0000

    bukatuta

--- app.js
+++ app.js
@@ -13,7 +13,22 @@ app.get("/", function(req, res) {
 });
 
 app.post("/bezeroa", function(req,res) {
-    res.send(`Kaixo ${req.body.izena}, eskaera jaso dut`);
+   let emaitza=[
+    {
+        "name":req.body.izena,
+        "email":req.body.email,
+        "password":req.body.password,
+        "confirm-password":req.body.ConfirmPassword,
+        "gender":req.body.gender,
+        "data":req.body.birth,
+        "address":req.body.address,
+        "city":req.body.city,
+        "zip":req.body.ZipCode,
+        "agree":req.body.conditions,
+        "newsletter":req.body.newsletter
+    }
+   ] 
+    res.send(emaitza);
 });
 
 app.get('/bezeroa/', function(req,res) {
